06c17cf788686f5336470865bc845d5987daff10
sggg2408 <59368631+sggg2408@users.noreply.github.com>
2025-03-30 09:00:59 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,35 @@
-from my_app import app
+service: flask-wsgi-api
 
-def handler(event, context):
-    from serverless_wsgi import handle_request
-    return handle_request(app, event, context)
+provider:
+  name: aws
+  runtime: python3.9
+  stage: ${opt:stage, 'dev'}
+  region: ap-southeast-1
+  iam:
+    role: arn:aws:iam::405125190450:role/LookerActionExecutionRole
+  logs:
+    restApi: true
+  lambdaHashingVersion: 20201221
+  tags:
+    Ownership: Data-Science
+    Environment: ${opt:stage, 'dev'}
+  apiGateway:
+    shouldStartNameWithService: true
+
+functions:
+  app:
+    handler: wsgi.handler
+    events:
+      - http: ANY /
+      - http: 'ANY /{proxy+}'
+
+plugins:
+  - serverless-python-requirements
+  - serverless-wsgi
+
+custom:
+  wsgi:
+    app: my_app.app
+    packRequirements: false
+  pythonRequirements:
+    dockerizePip: non-linux
